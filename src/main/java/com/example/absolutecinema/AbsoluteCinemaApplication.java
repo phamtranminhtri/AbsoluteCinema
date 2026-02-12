cf5b2d0046c7e903f7c5627b93b2bd45c316be59
Pham Tran Minh Tri <tri.phamtranminh@hcmut.edu.vn>
2026-02-12 16:01:00 +0700

    initi

---
+++ src/main/java/com/example/absolutecinema/AbsoluteCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.absolutecinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsoluteCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbsoluteCinemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/absolutecinema/AbsoluteCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.absolutecinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsoluteCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
